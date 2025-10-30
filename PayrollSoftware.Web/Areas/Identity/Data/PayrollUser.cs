62e6c754fc69b0c9c133aba99a98b8f751c98042
Somu-010 <22103441@iubat.edu>
2025-10-30 07:50:52 +0600

    layout, registration,login

---
+++ PayrollSoftware.Web/Areas/Identity/Data/PayrollUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PayrollProject.Areas.Identity.Data
+{
+    public class PayrollUser : IdentityUser
+    {
+        [PersonalData]
+        public string? FirstName { get; set; }
+
+        [PersonalData]
+        public string? LastName { get; set; }
+    }
+}
